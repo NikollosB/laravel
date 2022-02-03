8f3a2068a205d5c819a86a5d6431be7a707a5341
NikollosB <nicu.brinzila@gmail.com>
2022-02-03 11:38:16 +0200

    phpfull

---
+++ resources/views/broow.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP only</title>
+    <style>
+         body{
+        background-color:black;
+        color:green;
+        font-family: consolas;
+        
+    }
+    a{
+        text-decoration: none; 
+        color:green;
+    }
+        </style>
+</head>
+<body>
+<?php 
+echo 'Salut aici e PHP'
+?>
+
+
+<ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contacts">Contacts</a></li>
+        <li><a href="/nicuu">Nicuu</a></li>
+</ul>
+
+
+</body>
+</html>
+
+
